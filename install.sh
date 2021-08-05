334b46401623a079e33c6b9774fb7f9d91a0dfde
HAXA-XC <86255713+EBLETSM@users.noreply.github.com>
2021-08-06 01:08:32 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#---EBLIS---#
+echo "EBLIS install"
+echo "<-----------install Run---------->"
+pip install os
+pip install sys
+pip install socket
+pip install sh
+pip install colorama
+pip install datetime
+pip	install requests
+echo "<----------install End----------->"
+echo "OK install End"
+clear
+python3 XC.py
